3abeef9d8d04c7bb5f3c4c80e5aa1735a671fd34
Romchik38 <pomahehko.c@gmail.com>
2023-10-01 11:39:18 +0300

    finish index.php

--- index.php
+++ index.php
@@ -22,38 +22,60 @@
  * 
  * http://localhost:8000/some or /
  * must return 404 Not Found
- * 
  */
 
-include __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/example/bootstrap.php';
 
 use Example\App;
 
+/**
+ * REQUEST_URI => route
+ * 
+ * @param string $path REQUEST_URI
+ * 
+ * @return string
+ */
 function parseUrl(string $path): string
 {
     $result = pathinfo($path, PATHINFO_FILENAME);
     if (gettype($result) === 'string') {
-        if (strlen($result > 0)) return $result;
+        if (strlen($result > 0)) {
+            return $result;
+        }
     }
     return "";
 }
 
+/**
+ * Finish programm process with code and response
+ * 
+ * @param int    $code 
+ * @param string $message Message to display.
+ *                        Default ''
+ * 
+ * @return void
+ */
 function send(int $code, string $message = ''): void
 {
     $defaultAnswers = [
         404 => '404 Not Found'
     ];
     $response = '';
-    if (strlen($message) === 0) $response = $defaultAnswers[$code];
-    else $response = $message;
+    if (strlen($message) === 0) {
+        $response = $defaultAnswers[$code];
+    } else {
+        $response = $message;
+    }
     echo $response;
     http_response_code($code);
 }
 
 try {
     $exist = key_exists('REQUEST_URI', $_SERVER);
-    if ($exist === false) exit('Program must run via php-fpm server');
+    if ($exist === false) {
+        exit('Program must run via php-fpm server');
+    }
     $path = $_SERVER['REQUEST_URI'];
     $route = parseUrl($path);
     if (strlen($route) === 0) {
@@ -61,11 +83,14 @@ try {
         exit();
     }
     $app = new App($container);
-    $response = $app->run([
-        'route' => $route
-    ]);
-    if ($response === Null) send(404);
-    else send(200, $response);
+    $response = $app->run(
+        ['route' => $route]
+    );
+    if ($response === null) {
+        send(404);
+    } else {
+        send(200, $response);
+    }
 } catch (\Exception $e) {
     echo $e->getMessage() . PHP_EOL;
     send(500, '500 - server error');
