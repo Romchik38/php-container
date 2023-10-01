f1746ed0a717d4a357967a7326eb1d57557b1bbe
Romchik38 <pomahehko.c@gmail.com>
2023-10-01 11:18:42 +0300

    finished console.php

--- console.php
+++ console.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 /**
  * Example to use with console
@@ -14,13 +16,27 @@
  * @license MIT https://opensource.org/license/mit/
  * 
  * @link no link
+ * 
+ * Usage
+ * 
+ * "php console.php one" must print "1"
+ * "php console.php some" must print "Not Fount"
  */
 
+$app = include __DIR__ . '/example/bootstrap.php';
+
 try {
-    $app = include __DIR__ . '/example/bootstrap.php';
-    $route = 'one';
-    $result = $app->run(['route' => $route]);    
-    echo $result;
+    if ($argc < 2) {
+        printf('No arguments was passed to script');
+        exit(0);
+    };
+    $route = $argv[1];
+    $result = $app->run(['route' => $route]);
+    if ($result === null) {
+        printf('Not Found');
+    } else {
+        printf($result);
+    }
 } catch (\Exception $e) {
     echo 'App error:';
     echo $e->getMessage() . PHP_EOL;
