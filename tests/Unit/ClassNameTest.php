581be952fa045192ea83787438a47a8d488d342a
Romchik38 <pomahehko.c@gmail.com>
2025-03-13 16:28:23 +0200

    tests: added test for ClassName

---
+++ tests/Unit/ClassNameTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romchik38\Tests\Unit;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Romchik38\Container\ClassName;
+use stdClass;
+
+final class ClassNameTest extends TestCase
+{
+    public function testConstructNotExistingClass(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new ClassName('Not exist');
+    }
+
+    public function testConstruct(): void
+    {
+        $someClass = new stdClass();
+        $cl = new ClassName($someClass::class);
+        $this->assertSame($someClass::class, $cl->className);
+    }
+}
